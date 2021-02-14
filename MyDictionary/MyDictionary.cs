5a7d707ec2ba8ab85cc3bed60831c0f0fa8a6c05
AHMET EMİR ÖZPOLAT <l9120016@std.yildiz.edu.tr>
2021-02-15 00:58:27 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDict <K,V>
+    {
+        K[] Key;
+        V[] Value;
+
+        K[] tempKey;
+        V[] tempValue;
+        public MyDict()
+        {
+            Key = new K[0];
+            Value = new V[0];
+        }
+        public void Add(K item1, V item2)
+        {
+            tempKey = Key;
+            tempValue = Value;
+            Key = new K[Key.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                Key[i] = tempKey[i];
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                Value[i] = tempValue[i];
+            }
+
+        }
+
+        public int Count
+        {
+            get { return Key.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary1 = new Dictionary<int, string>();
+            dictionary1.Add(02, "Adıyaman");
+            dictionary1.Add(34, "İstanbul");
+            Console.WriteLine("Dictionary");
+            Console.WriteLine(dictionary1.Count);
+
+            MyDict<int, string> dictionary2 = new MyDict<int, string>{ };
+            dictionary2.Add(01, "Adana");
+            dictionary2.Add(35, "İzmir");
+            Console.WriteLine("MyDictionary");
+            Console.WriteLine(dictionary2.Count);
+
+        }
+
+    }
+}
